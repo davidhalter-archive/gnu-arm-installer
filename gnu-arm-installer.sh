5a0f3a0b31e92feff29b795b3485fbabd57d3c3a
ngamsaad.waipot <ngamsaad.waipot@ae0df2b8-a31b-11dd-a8c6-677c70d10fe4>
2008-11-10 10:21:41 +0000

    support the latest sources :
    binutils-2.19.tar.b2
    gcc-4.3.2.tar.bz2
    newlib-1.16.0.tar.gz
    insight-6.8.tar.bz2
    
    git-svn-id: http://hobbycode.googlecode.com/svn/trunk/gnu-arm-installer@10 ae0df2b8-a31b-11dd-a8c6-677c70d10fe4

--- gnu-arm-installer.sh
+++ gnu-arm-installer.sh
@@ -5,19 +5,19 @@ SRCDIR=$ROOT/src
 BUILDDIR=$ROOT/build
 PREFIX=$ROOT/install
 
-GCC_URL=http://mirror-fpt-telecom.fpt.net/gcc/releases/gcc-4.2.2/gcc-core-4.2.2.tar.bz2
-GCC_VERSION=4.2.2
+GCC_SRC=gcc-4.3.2.tar.bz2
+GCC_VERSION=4.3.2
 GCC_DIR=gcc-$GCC_VERSION
 
-BINUTILS_URL=http://ftp.gnu.org/gnu/binutils/binutils-2.18.tar.bz2
-BINUTILS_VERSION=2.18
+BINUTILS_SRC=binutils-2.19.tar.bz2
+BINUTILS_VERSION=2.19
 BINUTILS_DIR=binutils-$BINUTILS_VERSION
 
-NEWLIB_URL=ftp://sources.redhat.com/pub/newlib/newlib-1.15.0.tar.gz
-NEWLIB_VERSION=1.15.0
+NEWLIB_SRC=newlib-1.16.0.tar.gz
+NEWLIB_VERSION=1.16.0
 NEWLIB_DIR=newlib-$NEWLIB_VERSION
 
-INSIGHT_URL=http://mirror-fpt-telecom.fpt.net/sourceware/insight/releases/insight-6.8.tar.bz2
+INSIGHT_SRC=insight-6.8.tar.bz2
 INSIGHT_VERSION=6.8
 INSIGHT_DIR=insight-$INSIGHT_VERSION
 
@@ -33,16 +33,6 @@ read IGNORE
 #
 # Helper functions.
 #
-ensure_source()
-{
-    URL=$1
-    FILE=$(basename $1)
-
-    if [ ! -e $FILE ]; then
-	wget -O$FILE $URL
-    fi
-}
-
 unpack_source()
 {
 (
@@ -65,17 +55,11 @@ unpack_source()
 (
 cd $SRCDIR
 
-# First grab all the source files...
-ensure_source $GCC_URL
-ensure_source $BINUTILS_URL
-ensure_source $NEWLIB_URL
-ensure_source $INSIGHT_URL
-
 # ... And unpack the sources.
-unpack_source $(basename $GCC_URL)
-unpack_source $(basename $BINUTILS_URL)
-unpack_source $(basename $NEWLIB_URL)
-unpack_source $(basename $INSIGHT_URL)
+unpack_source $(basename $GCC_SRC)
+unpack_source $(basename $BINUTILS_SRC)
+unpack_source $(basename $NEWLIB_SRC)
+unpack_source $(basename $INSIGHT_SRC)
 )
 
 # Set the PATH to include the binaries we're going to build.
@@ -90,19 +74,7 @@ export PATH=$PREFIX/bin:$PATH
 # First we need to patch binutils, because makeinfo 4.11 fails the
 # autoconf check.
 cd $SRCDIR/$BINUTILS_DIR
-patch -p0 <<"EOF"
---- configure~ 2007-10-10 22:14:56.000000000 +1300
-+++ configure 2007-10-10 22:14:56.000000000 +1300
-@@ -3680,7 +3680,7 @@
-     # For an installed makeinfo, we require it to be from texinfo 4.4 or
-     # higher, else we use the "missing" dummy.
-     if ${MAKEINFO} --version \
--       | egrep 'texinfo[^0-9]*([1-3][0-9]|4\.[4-9]|[5-9])' >/dev/null 2>&1; then
-+       | egrep 'texinfo[^0-9]*([1-3][0-9]|4\.([4-9]|[1-9][0-9])|[5-9])' >/dev/null 2>&1; then
-       :
-     else
-       MAKEINFO="$MISSING makeinfo"
-EOF
+
 ) || exit 1
 
 # Now, build it.
@@ -110,7 +82,7 @@ mkdir -p $BUILDDIR/$BINUTILS_DIR
 cd $BUILDDIR/$BINUTILS_DIR
 
 $SRCDIR/$BINUTILS_DIR/configure --target=arm-elf --prefix=$PREFIX \
-    --enable-interwork --enable-multilib \
+    --enable-interwork --enable-multilib --with-float=soft \
     && make all install
 ) || exit 1
 
@@ -131,8 +103,8 @@ mkdir -p $BUILDDIR/$GCC_DIR
 cd $BUILDDIR/$GCC_DIR
 
 $SRCDIR/$GCC_DIR/configure --target=arm-elf --prefix=$PREFIX \
-    --enable-interwork --enable-multilib \
-    --enable-languages="c" --with-newlib \
+    --enable-interwork --enable-multilib --with-float=soft \
+    --enable-languages="c,c++" --with-newlib \
     --with-headers=$SRCDIR/$NEWLIB_DIR/newlib/libc/include \
     && make all-gcc install-gcc
 ) || exit 1
@@ -144,19 +116,7 @@ $SRCDIR/$GCC_DIR/configure --target=arm-elf --prefix=$PREFIX \
 (
 # Same issue, we have to patch to support makeinfo >= 4.11.
 cd $SRCDIR/$NEWLIB_DIR
-patch -p0 <<"EOF"
---- configure~ 2007-10-10 22:14:56.000000000 +1300
-+++ configure 2007-10-10 22:14:56.000000000 +1300
-@@ -3680,7 +3680,7 @@
-     # For an installed makeinfo, we require it to be from texinfo 4.4 or
-     # higher, else we use the "missing" dummy.
-     if ${MAKEINFO} --version \
--       | egrep 'texinfo[^0-9]*([1-3][0-9]|4\.[4-9]|[5-9])' >/dev/null 2>&1; then
-+       | egrep 'texinfo[^0-9]*([1-3][0-9]|4\.([4-9]|[1-9][0-9])|[5-9])' >/dev/null 2>&1; then
-       :
-     else
-       MAKEINFO="$MISSING makeinfo"
-EOF
+
 ) || exit 1
 
 # And now we can build it.
@@ -164,7 +124,7 @@ mkdir -p $BUILDDIR/$NEWLIB_DIR
 cd $BUILDDIR/$NEWLIB_DIR
 
 $SRCDIR/$NEWLIB_DIR/configure --target=arm-elf --prefix=$PREFIX \
-    --enable-interwork --enable-multilib \
+    --enable-interwork --enable-multilib --with-float=soft \
     && make all install
 ) || exit 1
 
@@ -187,7 +147,7 @@ mkdir -p $BUILDDIR/$INSIGHT_DIR
 cd $BUILDDIR/$INSIGHT_DIR
 
 $SRCDIR/$INSIGHT_DIR/configure --target=arm-elf --prefix=$PREFIX \
-    --enable-interwork --enable-multilib \
+    --enable-interwork --enable-multilib --with-float=soft \
     && make all install
 ) || exit 1
 
